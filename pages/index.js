7f98d3bca6bff235f07f660a72adf25c1262e528
Lazaro Machado <lazarosouzinha@gmail.com>
2026-02-08 19:50:18 -0300

    f m

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() { // This is the default page component for the home page
+    return (
+        <div>
+            <h1>Welcome to Next.js</h1>
+            <p>Get started by editing pages/index.js</p>
+        </div>
+    );
+}
